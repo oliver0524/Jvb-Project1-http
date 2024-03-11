34fd42ae7d7a75e256a7347208d3f3874e9d2827
oliver0524 <allabarry@aol.com>
2024-03-11 10:46:51 -0400

    Add changes to work with react

--- src/main/java/org/example/Controller/ProductController.java
+++ src/main/java/org/example/Controller/ProductController.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
+import io.javalin.http.Header;
 import org.example.Exception.ProductInfoException;
 import org.example.Exception.SellerException;
 import org.example.Model.ProductInfo;
@@ -33,12 +34,27 @@ public class ProductController {
     public Javalin getAPI(){
         Javalin api = Javalin.create();
 
+        api.before (ctx -> {
+            ctx.header("Access-Control-Allow-Origin", "*");
+            ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            ctx.header("Access-Control-Allow-Headers", "*");
+        });
+
+        //Javalin to handle preflight requests (sent via OPTIONS)
+        api.options("/*", ctx -> {
+                    ctx.header(Header.ACCESS_CONTROL_ALLOW_ORIGIN, "http://localhost:3000");
+                    ctx.header(Header.ACCESS_CONTROL_ALLOW_METHODS, "GET, POST, PUT, DELETE, OPTIONS");
+                    ctx.header(Header.ACCESS_CONTROL_ALLOW_HEADERS, "Content-Type, Authorization");
+                    ctx.status(200);
+                });
+
         api.get("/health",
                 context ->
                 {
                     context.result("the server is UP");
                 }
         );
+
         api.get("/seller", context -> {
             Set<String> sellerSet = sellerService.getAllSellers();
             context.json(sellerSet);
